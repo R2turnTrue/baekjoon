4f5453dbf407df85955226bc8d822c539bff3801
R2turnTrue <46389044+R2turnTrue@users.noreply.github.com>
2024-03-25 22:23:10 +0900

    SOLVE

--- 1654.py
+++ 1654.py
@@ -2,4 +2,20 @@ K, N = map(int, input().split())
 
 cables = [int(input()) for i in range(K)]
 
-print(cables)
+start = 1
+end = max(cables)
+
+while start <= end:
+    mid = (start + end) // 2
+
+    cnt = 0
+
+    for cable in cables:
+        cnt += cable // mid
+    
+    if cnt < N:
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(start - 1)
